1379d95d5ecfff26479931f94fa5d659452e87a5
Ryan Joarles <ryanjoarles@gmail.com>
2022-03-28 21:12:31 -0300

    Primeiro contato com Django finalizado
    Terminei minha primeira aula sobre Django.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+
 from . import views
-#from . significa que estou importando tudo o que tem dentro de views.py
+
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index')
-]
+    path('', views.index, name='index'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote')
+    ]
+
